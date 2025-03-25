e0f91d9b40e905e2002499d7348a06309523b3b5
Zankimen <mayyas.md4@gmail.com>
2025-03-26 06:26:09 +0700

    edit halaman laporan dan side bar dashboard event organizer

--- src/app/organizer/page.tsx
+++ src/app/organizer/page.tsx
@@ -251,7 +251,7 @@ export default function AdminDashboard() {
           {/* Welcome Section */}
           <div className="mb-8">
             <h1 className="text-2xl font-bold text-gray-800">Home</h1>
-            <p className="text-gray-600">Welcome to BookIt</p>
+            <p className="text-gray-600">Selamat datang di BookIt</p>
           </div>
 
           {/* Stats Cards */}
