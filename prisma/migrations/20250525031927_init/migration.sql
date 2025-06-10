1bef5121de287526c6813b9fa2a807a5d4a145c6
oodinc <masoodinc1@gmail.com>
2025-06-10 09:24:44 +0700

    feat: update dependencies and modify database schema for payment and ticket management

---
+++ prisma/migrations/20250525031927_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `invoice_id` on the `Pembayaran` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Pembayaran" DROP COLUMN "invoice_id";
